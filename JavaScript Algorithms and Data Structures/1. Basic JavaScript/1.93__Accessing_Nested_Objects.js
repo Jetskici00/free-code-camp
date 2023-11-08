83deae2150c28f1f38e3445905fcaa948d23eae7
jetski.ci <gidrokisik@gmail.com>
2023-11-08 10:44:32 +0500

    js

---
+++ JavaScript Algorithms and Data Structures/1. Basic JavaScript/1.93__Accessing_Nested_Objects.js
@@ -0,0 +1,15 @@
+var myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+console.log(gloveBoxContents);
